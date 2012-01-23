5a11642746811668c38f46fa83f64baeefab84ae
Benjamin <beneran89@gmail.com>
2012-01-23 18:40:53 -0500

    Made Game Engine class.

---
+++ GameEngine/GameEngine.py
@@ -0,0 +1,17 @@
+'''
+Created on Jan 23, 2012
+
+@author: Eran-Tasker
+'''
+
+class GameEngine(object):
+    '''
+    This class acts as the connection between the game model and the graphic engine.
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
