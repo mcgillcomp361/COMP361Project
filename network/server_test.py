1999b83c5934bea6fbdeff4c6897c3b6d668c5eb
Benjamin <beneran89@gmail.com>
2012-03-16 00:47:35 -0400

    tests

--- network/server_test.py
+++ network/server_test.py
@@ -6,9 +6,11 @@ Created on Feb 16, 2012
 from server_and_client import Server
 
 Server = Server()
+'''
 while True:
     if Server.activeConnections and time != 0:
         Server.broadCast(Server.activeConnections[0])
 
 Server.terminateAllConnection()
+'''
 run()
