9e9f47d012cddbb8edfb16f9a63a1d585d9eb392
muadibbm <muadibbm@yahoo.com>
2012-04-22 16:07:24 -0400

    as requested by Ben, added the multiplayer options to the main menu and their related "empty" functions in the "no-one-should-modify-except-julie" main menu class.

--- gui/menu.py
+++ gui/menu.py
@@ -37,7 +37,7 @@ class Menu():
                         self.screenImages.find('**/multiplayer_over'), 
                         self.screenImages.find('**/multiplayer_over'), 
                         self.screenImages.find('**/multiplayer')), frameColor=(1, 1, 1, 0), text_fg=(1, 1, 1, 1),
-                      pos=(1.3, 0, 0.15),parent=self.mainFrame, command = self.startGame, clickSound=self.menu_click,rolloverSound=self.menu_rollover)
+                      pos=(1.3, 0, 0.15),parent=self.mainFrame, command = self.multiplayer, clickSound=self.menu_click,rolloverSound=self.menu_rollover)
         b2.setScale(0.6,1,0.16)
         self.screenImages=loader.loadModel('./models/gui/tutorial.egg')
         b3 = DirectButton(geom=(self.screenImages.find('**/tutorial'), 
@@ -81,25 +81,56 @@ class Menu():
         self.mainFrame.destroy()
         self.b.destroy()
         gameEngine.initialize()
+        
+    def hostGame(self):
+        print 'Waiting for other players'
+        
+    def joinGame(self):
+        print 'Looking for a match'
 
     def mainMenu(self):
         self.mainFrame.destroy()
         
-        b1 = DirectButton(text = ("Single Player", "click!", "start_roll", "disabled"), frameColor=(1, 1, 1, 0), text_fg=(1, 1, 1, 1),
-                     text_scale=0.1, text_align = TextNode.ALeft, pos=(0.3, 0, 0.4),
+        self.screenImages=loader.loadModel('./models/gui/singleplayer.egg')
+        b1 = DirectButton(geom=(self.screenImages.find('**/singleplayer'), 
+                        self.screenImages.find('**/singleplayer_over'), 
+                        self.screenImages.find('**/singleplayer_over'), 
+                        self.screenImages.find('**/singleplayer')), frameColor=(1, 1, 1, 0), text_fg=(1, 1, 1, 1),
+                     text_scale=0.1, text_align = TextNode.ALeft, pos=(0.6, 0, 0.4),
                      relief=2, command = self.startGame, clickSound=self.menu_click,rolloverSound=self.menu_rollover)
-        b2 = DirectButton(text = ("Multiplayer", "click!", "start_roll", "disabled"), frameColor=(1, 1, 1, 0), text_fg=(1, 1, 1, 1),
-                      text_scale=0.1,  text_align = TextNode.ALeft,pos=(1, 0, 0.15),
-                      relief=2, command = self.startGame, clickSound=self.menu_click,rolloverSound=self.menu_rollover)
-        b3 = DirectButton(text = ("Tutorial", "click!", "tutorial_roll", "disabled"), frameColor=(1, 1, 1, 0), text_fg=(1, 1, 1, 1),
-                      text_scale=0.1, text_align = TextNode.ALeft,pos=(-1.2, 0, 0.1),
+        b1.setScale(0.6,1,0.16)
+        self.screenImages=loader.loadModel('./models/gui/multiplayer.egg')
+        b2 = DirectButton(geom=(self.screenImages.find('**/multiplayer'), 
+                        self.screenImages.find('**/multiplayer_over'), 
+                        self.screenImages.find('**/multiplayer_over'), 
+                        self.screenImages.find('**/multiplayer')), frameColor=(1, 1, 1, 0), text_fg=(1, 1, 1, 1),
+                      text_scale=0.1,  text_align = TextNode.ALeft,pos=(1.3, 0, 0.15),
+                      relief=2, command = self.multiplayer, clickSound=self.menu_click,rolloverSound=self.menu_rollover)
+        b2.setScale(0.6,1,0.16)
+        self.screenImages=loader.loadModel('./models/gui/tutorial.egg')
+        b3 = DirectButton(geom=(self.screenImages.find('**/tutorial'), 
+                        self.screenImages.find('**/tutorial_over'), 
+                        self.screenImages.find('**/tutorial_over'), 
+                        self.screenImages.find('**/tutorial')), frameColor=(1, 1, 1, 0), text_fg=(1, 1, 1, 1),
+                      text_scale=0.1, text_align = TextNode.ALeft,pos=(-1.1, 0, 0.1),
                       relief=2, command = self.tutorial, clickSound=self.menu_click,rolloverSound=self.menu_rollover)
-        b4 = DirectButton(text = ("Settings", "click!", "settings_roll", "disabled"), frameColor=(1, 1, 1, 0), text_fg=(1, 1, 1, 1),
-                              text_scale=0.1, text_align = TextNode.ALeft,pos=(0.35, 0, -0.15),
+        b3.setScale(0.6,1,0.16)
+        self.screenImages=loader.loadModel('./models/gui/settings.egg')
+        b4 = DirectButton(geom=(self.screenImages.find('**/settings'), 
+                        self.screenImages.find('**/settings_over'), 
+                        self.screenImages.find('**/settings_over'), 
+                        self.screenImages.find('**/settings')), frameColor=(1, 1, 1, 0), text_fg=(1, 1, 1, 1),
+                              text_scale=0.1, text_align = TextNode.ALeft,pos=(0.45, 0, -0.1),
                               relief=2, command = self.settings, clickSound=self.menu_click,rolloverSound=self.menu_rollover)
-        b5 = DirectButton(text = ("Exit", "click!", "exit", "disabled"), frameColor=(1, 1, 1, 0), text_fg=(1, 1, 1, 1),
-                              text_scale=0.1, text_align = TextNode.ALeft,pos=(-1.55, 0, -0.35),
+        b4.setScale(0.6,1,0.16) 
+        self.screenImages=loader.loadModel('./models/gui/exit.egg')
+        b5 = DirectButton(geom=(self.screenImages.find('**/exit'), 
+                        self.screenImages.find('**/exit_over'), 
+                        self.screenImages.find('**/exit_over'), 
+                        self.screenImages.find('**/exit')), frameColor=(1, 1, 1, 0), text_fg=(1, 1, 1, 1),
+                              text_scale=0.1, text_align = TextNode.ALeft,pos=(-1.4, 0, -0.35),
                               relief=2, command = sys.exit,rolloverSound=self.menu_rollover)
+        b5.setScale(0.6,1,0.16)
             
         self.mainFrame = DirectFrame(pos=(0,0,0) )
         b1.reparentTo(self.mainFrame)
@@ -112,19 +143,38 @@ class Menu():
     def tutorial(self):
         initial_menu = 1
         self.mainFrame.destroy()
-        screenText = OnscreenText(text = 'TUTORIAL', pos = (0.2, 0.6), scale = 0.1, fg = (1, 1, 1, 1))
-        b1 = DirectButton(text = ("Back to Main Menu", "click!", "main menu roll", "disabled"), frameColor=(1, 1, 1, 0), text_fg=(1, 1, 1, 1),
+        screenText = OnscreenText(text = 'Tutorial', pos = (0.2, 0.6), scale = 0.1, fg = (1, 1, 1, 1))
+        b1 = DirectButton(text = ("Back to Main Menu"), frameColor=(1, 1, 1, 0), text_fg=(1, 1, 1, 1),
                   text_scale=0.1, text_align = TextNode.ALeft, pos=(0.2, 0, -0.5),
                   relief=2, command = self.mainMenu, clickSound=self.menu_click,rolloverSound=self.menu_rollover)
         self.mainFrame = DirectFrame(pos=(0,0,0))       
         screenText.reparentTo(self.mainFrame)
         b1.reparentTo(self.mainFrame)
+        
+    def multiplayer(self):
+        initial_menu = 1
+        self.mainFrame.destroy()
+        screenText = OnscreenText(text = 'Multiplayer', pos = (0.2, 0.6), scale = 0.1, fg = (1, 1, 1, 1))
+        b1 = DirectButton(text = ("Back to Main Menu"), frameColor=(1, 1, 1, 0), text_fg=(1, 1, 1, 1),
+                  text_scale=0.1, text_align = TextNode.ALeft, pos=(0.2, 0, -0.5),
+                  relief=2, command = self.mainMenu, clickSound=self.menu_click,rolloverSound=self.menu_rollover)
+        b2 = DirectButton(text = ("Host Game"), frameColor=(1, 1, 1, 0), text_fg=(1, 1, 1, 1),
+                              text_scale=0.1, text_align = TextNode.ALeft,pos=(0.3, 0, 0.4),
+                              relief=2, command = self.hostGame, clickSound=self.menu_click,rolloverSound=self.menu_rollover)
+        b3 = DirectButton(text = ("Join Game"), frameColor=(1, 1, 1, 0), text_fg=(1, 1, 1, 1),
+                              text_scale=0.1, text_align = TextNode.ALeft,pos=(0.8, 0, 0.15),
+                              relief=2, command = self.joinGame,rolloverSound=self.menu_rollover)
+        self.mainFrame = DirectFrame(pos=(0,0,0))     
+        screenText.reparentTo(self.mainFrame)
+        b1.reparentTo(self.mainFrame)
+        b2.reparentTo(self.mainFrame)
+        b3.reparentTo(self.mainFrame)
     
     def settings(self):
         self.mainFrame.destroy()
         screenText = OnscreenText(text = 'Audio Settings', pos = (0.2, 0.6), scale = .1, fg = (1, 1, 1, 1))
         volumeText = OnscreenText(text = 'Set Volume:', pos = (0.1, 0.5), scale = 0.05, fg = (1, 1, 1, 1))
-        b1 = DirectButton(text = ("Back to Main Menu", "click!", "main menu roll", "disabled"), frameColor=(1, 1, 1, 0), text_fg=(1, 1, 1, 1),
+        b1 = DirectButton(text = ("Back to Main Menu"), frameColor=(1, 1, 1, 0), text_fg=(1, 1, 1, 1),
                   text_scale=0.1, text_align = TextNode.ALeft, pos=(0.2, 0, -0.5),
                   relief=2, command = self.mainMenu, clickSound=self.menu_click,rolloverSound=self.menu_rollover)
  
