72aff9b20c6a8d0c0e41411c6dd6f821c2754c92
dragontamerV2 <beneran89@gmail.com>
2012-04-23 16:38:39 -0400

    some work

--- distributedNetwork/client.py
+++ distributedNetwork/client.py
@@ -20,3 +20,14 @@ class MyClientRepository(ClientRepository):
 class Client(DirectObject):
     def __init__(self):
         DirectObject.__init__(self)
+        
+        #No player yet
+        self.player = None
+        
+        tcpPort = base.config.GetInt('server-port', 4400)
+        hostname = base.config.GetString('server-host', '192.168.1.110')
+        self.url = URLSpec('http://%s:%s' % (hostname, tcpPort))
+        
+        self.cr = MyClientRepository()
+        self.cr.connect([self.url], successCallback = self.connectSuccess, failureCallback = self.connectFailure)
+        
